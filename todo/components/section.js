6bb370928227eb72bf47348083a40cb04fba6a17
Syahmi Fauzi <brosyahmifauzi@gmail.com>
2021-04-04 18:42:55 +0800

    fix tailwindcss used classes

--- todo/components/section.js
+++ todo/components/section.js
@@ -10,12 +10,21 @@ import {
 library.add(fas, faClipboardList, faList, faClipboardCheck);
 
 const Field = ({ icon, color, title, children }) => {
+  const getColor = () => {
+    const colors = {
+      blue: 'bg-blue-400',
+      yellow: 'bg-yellow-400',
+      green: 'bg-green-400',
+    };
+    return colors[color];
+  };
+
   return (
     <div className="rounded-sm shadow-md mt-16 py-2 px-4 border">
       <div className="flex items-center">
         {icon && (
           <div
-            className={`-mt-10 px-4 py-2 w-16 h-16 text-center rounded-md shadow-md text-white bg-${color}-400`}>
+            className={`-mt-10 px-4 py-2 w-16 h-16 text-center rounded-md shadow-md text-white ${getColor()}`}>
             <FontAwesomeIcon
               icon={['fas', `${icon}`]}
               className="w-full h-full text-4xl"
