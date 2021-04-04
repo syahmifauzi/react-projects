644af1af6f6fd7d578c58d02361264516cd15e1f
Syahmi Fauzi <brosyahmifauzi@gmail.com>
2021-04-04 20:08:09 +0800

    fix assets baseUrl

--- todo/next.config.js
+++ todo/next.config.js
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   future: {
     webpack5: true,
   },
   target: 'serverless',
+  assetPrefix: isProd ? 'https://todo-sf.netlify.app' : '',
 };
